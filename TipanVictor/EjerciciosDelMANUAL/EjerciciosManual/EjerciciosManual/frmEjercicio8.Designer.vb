ef254258a77d6e0c8a4c295ac248ce7d7c20bfe0
CarlosBrusilC <carlosbrusil@hotmail.com>
2018-11-07 22:56:50 -0500

    cambio a ejercicio8   errado
    se daño el ej 8

--- TipanVictor/EjerciciosDelMANUAL/EjerciciosManual/EjerciciosManual/frmEjercicio8.Designer.vb
+++ TipanVictor/EjerciciosDelMANUAL/EjerciciosManual/EjerciciosManual/frmEjercicio8.Designer.vb
@@ -22,35 +22,37 @@ Partial Class frmEjercicio8
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.lblTotal = New System.Windows.Forms.Label()
+        Me.lblPuntos = New System.Windows.Forms.Label()
         Me.Label9 = New System.Windows.Forms.Label()
-        Me.lblDescD = New System.Windows.Forms.Label()
+        Me.lblObs = New System.Windows.Forms.Label()
         Me.Label7 = New System.Windows.Forms.Label()
-        Me.lblDesPor = New System.Windows.Forms.Label()
-        Me.lblSubtotal = New System.Windows.Forms.Label()
-        Me.btnCalcular = New System.Windows.Forms.Button()
+        Me.lblPromedio = New System.Windows.Forms.Label()
+        Me.btnObtener = New System.Windows.Forms.Button()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.btnSalir = New System.Windows.Forms.Button()
-        Me.btnNuevo = New System.Windows.Forms.Button()
+        Me.btnOtro = New System.Windows.Forms.Button()
         Me.Label5 = New System.Windows.Forms.Label()
         Me.Label4 = New System.Windows.Forms.Label()
-        Me.txtCantComp = New System.Windows.Forms.TextBox()
+        Me.txtNota1 = New System.Windows.Forms.TextBox()
         Me.Label3 = New System.Windows.Forms.Label()
-        Me.txtPrecUnit = New System.Windows.Forms.TextBox()
+        Me.txtCurso = New System.Windows.Forms.TextBox()
         Me.Label2 = New System.Windows.Forms.Label()
-        Me.txtProducto = New System.Windows.Forms.TextBox()
+        Me.txtAlumno = New System.Windows.Forms.TextBox()
+        Me.lblCualidad = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.TxtNota2 = New System.Windows.Forms.TextBox()
         Me.SuspendLayout()
         '
-        'lblTotal
+        'lblPuntos
         '
-        Me.lblTotal.AutoSize = True
-        Me.lblTotal.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblTotal.Location = New System.Drawing.Point(262, 431)
-        Me.lblTotal.Margin = New System.Windows.Forms.Padding(5, 0, 5, 0)
-        Me.lblTotal.Name = "lblTotal"
-        Me.lblTotal.Size = New System.Drawing.Size(57, 20)
-        Me.lblTotal.TabIndex = 74
-        Me.lblTotal.Text = "Label7"
+        Me.lblPuntos.AutoSize = True
+        Me.lblPuntos.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblPuntos.Location = New System.Drawing.Point(262, 431)
+        Me.lblPuntos.Margin = New System.Windows.Forms.Padding(5, 0, 5, 0)
+        Me.lblPuntos.Name = "lblPuntos"
+        Me.lblPuntos.Size = New System.Drawing.Size(57, 20)
+        Me.lblPuntos.TabIndex = 74
+        Me.lblPuntos.Text = "Label7"
         '
         'Label9
         '
@@ -59,20 +61,20 @@ Partial Class frmEjercicio8
         Me.Label9.Location = New System.Drawing.Point(54, 431)
         Me.Label9.Margin = New System.Windows.Forms.Padding(5, 0, 5, 0)
         Me.Label9.Name = "Label9"
-        Me.Label9.Size = New System.Drawing.Size(160, 20)
+        Me.Label9.Size = New System.Drawing.Size(59, 20)
         Me.Label9.TabIndex = 73
-        Me.Label9.Text = "Total a pagar =====>"
+        Me.Label9.Text = "Puntos"
         '
-        'lblDescD
+        'lblObs
         '
-        Me.lblDescD.AutoSize = True
-        Me.lblDescD.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblDescD.Location = New System.Drawing.Point(262, 373)
-        Me.lblDescD.Margin = New System.Windows.Forms.Padding(5, 0, 5, 0)
-        Me.lblDescD.Name = "lblDescD"
-        Me.lblDescD.Size = New System.Drawing.Size(57, 20)
-        Me.lblDescD.TabIndex = 72
-        Me.lblDescD.Text = "Label7"
+        Me.lblObs.AutoSize = True
+        Me.lblObs.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblObs.Location = New System.Drawing.Point(262, 373)
+        Me.lblObs.Margin = New System.Windows.Forms.Padding(5, 0, 5, 0)
+        Me.lblObs.Name = "lblObs"
+        Me.lblObs.Size = New System.Drawing.Size(57, 20)
+        Me.lblObs.TabIndex = 72
+        Me.lblObs.Text = "Label7"
         '
         'Label7
         '
@@ -81,42 +83,31 @@ Partial Class frmEjercicio8
         Me.Label7.Location = New System.Drawing.Point(54, 373)
         Me.Label7.Margin = New System.Windows.Forms.Padding(5, 0, 5, 0)
         Me.Label7.Name = "Label7"
-        Me.Label7.Size = New System.Drawing.Size(100, 20)
+        Me.Label7.Size = New System.Drawing.Size(97, 20)
         Me.Label7.TabIndex = 71
-        Me.Label7.Text = "Descuento $"
+        Me.Label7.Text = "Observación"
         '
-        'lblDesPor
+        'lblPromedio
         '
-        Me.lblDesPor.AutoSize = True
-        Me.lblDesPor.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblDesPor.Location = New System.Drawing.Point(258, 314)
-        Me.lblDesPor.Margin = New System.Windows.Forms.Padding(5, 0, 5, 0)
-        Me.lblDesPor.Name = "lblDesPor"
-        Me.lblDesPor.Size = New System.Drawing.Size(57, 20)
-        Me.lblDesPor.TabIndex = 70
-        Me.lblDesPor.Text = "Label7"
+        Me.lblPromedio.AutoSize = True
+        Me.lblPromedio.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblPromedio.Location = New System.Drawing.Point(258, 314)
+        Me.lblPromedio.Margin = New System.Windows.Forms.Padding(5, 0, 5, 0)
+        Me.lblPromedio.Name = "lblPromedio"
+        Me.lblPromedio.Size = New System.Drawing.Size(57, 20)
+        Me.lblPromedio.TabIndex = 70
+        Me.lblPromedio.Text = "Label7"
         '
-        'lblSubtotal
+        'btnObtener
         '
-        Me.lblSubtotal.AutoSize = True
-        Me.lblSubtotal.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblSubtotal.Location = New System.Drawing.Point(258, 254)
-        Me.lblSubtotal.Margin = New System.Windows.Forms.Padding(5, 0, 5, 0)
-        Me.lblSubtotal.Name = "lblSubtotal"
-        Me.lblSubtotal.Size = New System.Drawing.Size(57, 20)
-        Me.lblSubtotal.TabIndex = 69
-        Me.lblSubtotal.Text = "Label6"
-        '
-        'btnCalcular
-        '
-        Me.btnCalcular.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnCalcular.Location = New System.Drawing.Point(444, 254)
-        Me.btnCalcular.Margin = New System.Windows.Forms.Padding(5)
-        Me.btnCalcular.Name = "btnCalcular"
-        Me.btnCalcular.Size = New System.Drawing.Size(148, 49)
-        Me.btnCalcular.TabIndex = 68
-        Me.btnCalcular.Text = "Calcular"
-        Me.btnCalcular.UseVisualStyleBackColor = True
+        Me.btnObtener.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnObtener.Location = New System.Drawing.Point(445, 314)
+        Me.btnObtener.Margin = New System.Windows.Forms.Padding(5)
+        Me.btnObtener.Name = "btnObtener"
+        Me.btnObtener.Size = New System.Drawing.Size(148, 49)
+        Me.btnObtener.TabIndex = 68
+        Me.btnObtener.Text = "Obtener Promedio"
+        Me.btnObtener.UseVisualStyleBackColor = True
         '
         'Label1
         '
@@ -125,9 +116,9 @@ Partial Class frmEjercicio8
         Me.Label1.Location = New System.Drawing.Point(50, 314)
         Me.Label1.Margin = New System.Windows.Forms.Padding(5, 0, 5, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(105, 20)
+        Me.Label1.Size = New System.Drawing.Size(76, 20)
         Me.Label1.TabIndex = 67
-        Me.Label1.Text = "Descuento %"
+        Me.Label1.Text = "Promedio"
         '
         'btnSalir
         '
@@ -140,16 +131,16 @@ Partial Class frmEjercicio8
         Me.btnSalir.Text = "Salir"
         Me.btnSalir.UseVisualStyleBackColor = True
         '
-        'btnNuevo
+        'btnOtro
         '
-        Me.btnNuevo.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnNuevo.Location = New System.Drawing.Point(443, 329)
-        Me.btnNuevo.Margin = New System.Windows.Forms.Padding(5)
-        Me.btnNuevo.Name = "btnNuevo"
-        Me.btnNuevo.Size = New System.Drawing.Size(149, 49)
-        Me.btnNuevo.TabIndex = 65
-        Me.btnNuevo.Text = "Nuevo "
-        Me.btnNuevo.UseVisualStyleBackColor = True
+        Me.btnOtro.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnOtro.Location = New System.Drawing.Point(444, 225)
+        Me.btnOtro.Margin = New System.Windows.Forms.Padding(5)
+        Me.btnOtro.Name = "btnOtro"
+        Me.btnOtro.Size = New System.Drawing.Size(149, 49)
+        Me.btnOtro.TabIndex = 65
+        Me.btnOtro.Text = "Otro Alumno"
+        Me.btnOtro.UseVisualStyleBackColor = True
         '
         'Label5
         '
@@ -158,9 +149,9 @@ Partial Class frmEjercicio8
         Me.Label5.Location = New System.Drawing.Point(50, 254)
         Me.Label5.Margin = New System.Windows.Forms.Padding(5, 0, 5, 0)
         Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(82, 20)
+        Me.Label5.Size = New System.Drawing.Size(85, 20)
         Me.Label5.TabIndex = 64
-        Me.Label5.Text = "Subtotal $"
+        Me.Label5.Text = "Nota N° 02"
         '
         'Label4
         '
@@ -169,18 +160,18 @@ Partial Class frmEjercicio8
         Me.Label4.Location = New System.Drawing.Point(52, 196)
         Me.Label4.Margin = New System.Windows.Forms.Padding(5, 0, 5, 0)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(148, 20)
+        Me.Label4.Size = New System.Drawing.Size(85, 20)
         Me.Label4.TabIndex = 63
-        Me.Label4.Text = "Cantidad comprada"
+        Me.Label4.Text = "Nota N° 01"
         '
-        'txtCantComp
+        'txtNota1
         '
-        Me.txtCantComp.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txtCantComp.Location = New System.Drawing.Point(262, 190)
-        Me.txtCantComp.Margin = New System.Windows.Forms.Padding(5)
-        Me.txtCantComp.Name = "txtCantComp"
-        Me.txtCantComp.Size = New System.Drawing.Size(118, 26)
-        Me.txtCantComp.TabIndex = 62
+        Me.txtNota1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtNota1.Location = New System.Drawing.Point(262, 190)
+        Me.txtNota1.Margin = New System.Windows.Forms.Padding(5)
+        Me.txtNota1.Name = "txtNota1"
+        Me.txtNota1.Size = New System.Drawing.Size(118, 26)
+        Me.txtNota1.TabIndex = 62
         '
         'Label3
         '
@@ -189,18 +180,18 @@ Partial Class frmEjercicio8
         Me.Label3.Location = New System.Drawing.Point(54, 133)
         Me.Label3.Margin = New System.Windows.Forms.Padding(5, 0, 5, 0)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(112, 20)
+        Me.Label3.Size = New System.Drawing.Size(51, 20)
         Me.Label3.TabIndex = 61
-        Me.Label3.Text = "Precio Unitario"
+        Me.Label3.Text = "Curso"
         '
-        'txtPrecUnit
+        'txtCurso
         '
-        Me.txtPrecUnit.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txtPrecUnit.Location = New System.Drawing.Point(264, 127)
-        Me.txtPrecUnit.Margin = New System.Windows.Forms.Padding(5)
-        Me.txtPrecUnit.Name = "txtPrecUnit"
-        Me.txtPrecUnit.Size = New System.Drawing.Size(118, 26)
-        Me.txtPrecUnit.TabIndex = 60
+        Me.txtCurso.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtCurso.Location = New System.Drawing.Point(264, 127)
+        Me.txtCurso.Margin = New System.Windows.Forms.Padding(5)
+        Me.txtCurso.Name = "txtCurso"
+        Me.txtCurso.Size = New System.Drawing.Size(118, 26)
+        Me.txtCurso.TabIndex = 60
         '
         'Label2
         '
@@ -209,41 +200,74 @@ Partial Class frmEjercicio8
         Me.Label2.Location = New System.Drawing.Point(52, 61)
         Me.Label2.Margin = New System.Windows.Forms.Padding(5, 0, 5, 0)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(73, 20)
+        Me.Label2.Size = New System.Drawing.Size(63, 20)
         Me.Label2.TabIndex = 59
-        Me.Label2.Text = "Producto"
+        Me.Label2.Text = "Alumno"
         '
-        'txtProducto
+        'txtAlumno
         '
-        Me.txtProducto.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txtProducto.Location = New System.Drawing.Point(262, 61)
-        Me.txtProducto.Margin = New System.Windows.Forms.Padding(5)
-        Me.txtProducto.Name = "txtProducto"
-        Me.txtProducto.Size = New System.Drawing.Size(330, 26)
-        Me.txtProducto.TabIndex = 58
+        Me.txtAlumno.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtAlumno.Location = New System.Drawing.Point(262, 61)
+        Me.txtAlumno.Margin = New System.Windows.Forms.Padding(5)
+        Me.txtAlumno.Name = "txtAlumno"
+        Me.txtAlumno.Size = New System.Drawing.Size(330, 26)
+        Me.txtAlumno.TabIndex = 58
+        '
+        'lblCualidad
+        '
+        Me.lblCualidad.AutoSize = True
+        Me.lblCualidad.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblCualidad.Location = New System.Drawing.Point(262, 535)
+        Me.lblCualidad.Margin = New System.Windows.Forms.Padding(5, 0, 5, 0)
+        Me.lblCualidad.Name = "lblCualidad"
+        Me.lblCualidad.Size = New System.Drawing.Size(57, 20)
+        Me.lblCualidad.TabIndex = 76
+        Me.lblCualidad.Text = "Label7"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(262, 483)
+        Me.Label8.Margin = New System.Windows.Forms.Padding(5, 0, 5, 0)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(71, 20)
+        Me.Label8.TabIndex = 75
+        Me.Label8.Text = "Cualidad"
+        '
+        'TxtNota2
+        '
+        Me.TxtNota2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TxtNota2.Location = New System.Drawing.Point(264, 248)
+        Me.TxtNota2.Margin = New System.Windows.Forms.Padding(5)
+        Me.TxtNota2.Name = "TxtNota2"
+        Me.TxtNota2.Size = New System.Drawing.Size(118, 26)
+        Me.TxtNota2.TabIndex = 77
         '
         'frmEjercicio8
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(667, 564)
-        Me.Controls.Add(Me.lblTotal)
+        Me.Controls.Add(Me.TxtNota2)
+        Me.Controls.Add(Me.lblCualidad)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.lblPuntos)
         Me.Controls.Add(Me.Label9)
-        Me.Controls.Add(Me.lblDescD)
+        Me.Controls.Add(Me.lblObs)
         Me.Controls.Add(Me.Label7)
-        Me.Controls.Add(Me.lblDesPor)
-        Me.Controls.Add(Me.lblSubtotal)
-        Me.Controls.Add(Me.btnCalcular)
+        Me.Controls.Add(Me.lblPromedio)
+        Me.Controls.Add(Me.btnObtener)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.btnSalir)
-        Me.Controls.Add(Me.btnNuevo)
+        Me.Controls.Add(Me.btnOtro)
         Me.Controls.Add(Me.Label5)
         Me.Controls.Add(Me.Label4)
-        Me.Controls.Add(Me.txtCantComp)
+        Me.Controls.Add(Me.txtNota1)
         Me.Controls.Add(Me.Label3)
-        Me.Controls.Add(Me.txtPrecUnit)
+        Me.Controls.Add(Me.txtCurso)
         Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.txtProducto)
+        Me.Controls.Add(Me.txtAlumno)
         Me.Name = "frmEjercicio8"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Venta de Productos"
@@ -251,21 +275,23 @@ Partial Class frmEjercicio8
         Me.PerformLayout()
 
     End Sub
-    Friend WithEvents lblTotal As System.Windows.Forms.Label
+    Friend WithEvents lblPuntos As System.Windows.Forms.Label
     Friend WithEvents Label9 As System.Windows.Forms.Label
-    Friend WithEvents lblDescD As System.Windows.Forms.Label
+    Friend WithEvents lblObs As System.Windows.Forms.Label
     Friend WithEvents Label7 As System.Windows.Forms.Label
-    Friend WithEvents lblDesPor As System.Windows.Forms.Label
-    Friend WithEvents lblSubtotal As System.Windows.Forms.Label
-    Friend WithEvents btnCalcular As System.Windows.Forms.Button
+    Friend WithEvents lblPromedio As System.Windows.Forms.Label
+    Friend WithEvents btnObtener As System.Windows.Forms.Button
     Friend WithEvents Label1 As System.Windows.Forms.Label
     Friend WithEvents btnSalir As System.Windows.Forms.Button
-    Friend WithEvents btnNuevo As System.Windows.Forms.Button
+    Friend WithEvents btnOtro As System.Windows.Forms.Button
     Friend WithEvents Label5 As System.Windows.Forms.Label
     Friend WithEvents Label4 As System.Windows.Forms.Label
-    Friend WithEvents txtCantComp As System.Windows.Forms.TextBox
+    Friend WithEvents txtNota1 As System.Windows.Forms.TextBox
     Friend WithEvents Label3 As System.Windows.Forms.Label
-    Friend WithEvents txtPrecUnit As System.Windows.Forms.TextBox
+    Friend WithEvents txtCurso As System.Windows.Forms.TextBox
     Friend WithEvents Label2 As System.Windows.Forms.Label
-    Friend WithEvents txtProducto As System.Windows.Forms.TextBox
+    Friend WithEvents txtAlumno As System.Windows.Forms.TextBox
+    Friend WithEvents lblCualidad As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents TxtNota2 As System.Windows.Forms.TextBox
 End Class
